5aa05e0066ae0d8d4f67e8bd605b6dd8ad460839
SamanthaW <s-waterburys@bsd405.org>
2025-03-03 14:04:35 -0800

    bed

--- Assets/Script/FlickerControl.cs
+++ Assets/Script/FlickerControl.cs
@@ -18,10 +18,10 @@ public class FlickerControl : MonoBehaviour
     {
         isFlickering = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.01f, 0.2f);
+        timeDelay = Random.Range(0.05f, 0.1f);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.1f, 0.2f);
+        timeDelay = Random.Range(0.05f, 0.1f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
     }
